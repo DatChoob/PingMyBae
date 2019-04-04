a4e580ce621c3c6c3296f287433a487615ab632f
Josh Singh <jashmit04@yahoo.com>
2019-04-04 14:52:27 -0700

    Making Login Page a stateless widget

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,89 +1,34 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ping_friends/google_signin_button.dart';
 import 'package:ping_friends/util/authentication.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => new _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  String _errorMessage;
-  bool _isLoading;
-
-  @override
-  void initState() {
-    _errorMessage = "";
-    _isLoading = false;
-    super.initState();
-  }
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ping My Friends Login Page'),
-      ),
-      body: Stack(
-        children: <Widget>[
-          _showBody(),
-          _showCircularProgress(),
-        ],
-      ),
-    );
-  }
-
-  Widget _showCircularProgress() {
-    if (_isLoading) {
-      return Center(child: CircularProgressIndicator());
-    }
-    return Container(width: 0, height: 0);
-  }
-
-  Widget _showBody() {
-    return Container(
-        padding: EdgeInsets.all(16.0),
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            _showLogo(),
-            _showLoginWithGoogleButton(),
-            //_showErrorMessage(),
-          ],
-        ));
-  }
-
-  Widget _showErrorMessage() {
-    if (_errorMessage.length > 0 && _errorMessage != null) {
-      return new Text(
-        _errorMessage,
-        style: TextStyle(
-            fontSize: 13.0,
-            color: Colors.red,
-            height: 1.0,
-            fontWeight: FontWeight.w300),
-      );
-    }
-  }
-
-  Widget _showLogo() {
-    return Hero(
-      tag: 'loginHero',
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 70.0),
-        child: FlutterLogo(
-          size: 200,
+        appBar: AppBar(
+          title: Text('Ping My Friends Login Page'),
         ),
-      ),
-    );
+        body: Container(
+            padding: EdgeInsets.all(16.0),
+            child: ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                Hero(
+                  tag: 'loginHero',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 70.0),
+                    child: FlutterLogo(
+                      size: 200,
+                    ),
+                  ),
+                ),
+                GoogleSignInButton(onPressed: signIn),
+              ],
+            )));
   }
 
   signIn() {
     authService.googleSignIn();
-  }
-
-  _showLoginWithGoogleButton() {
-    return GoogleSignInButton(onPressed: signIn);
   }
 }
